9c475c5b8d10965f97983b3acdaff93c4949436a
hoshit <hoshi@gclue.jp>
2013-04-02 15:45:55 +0900

    初回コミット.

--- src/com/example/surfaceview/SurfaceSample.java
+++ src/com/example/surfaceview/SurfaceSample.java
@@ -2,11 +2,10 @@ package com.example.surfaceview;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -20,14 +19,25 @@ public class SurfaceSample extends Activity {
 	}
 }
 
-class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback{
 
+	/**
+	 * サウンド再生データを保持します。
+	 */
+	private MediaPlayer mMp;
+	
 	public MySurfaceView(Context context) {
 		super(context);
 		Log.i("SURFACE","MySurfaceView()");
-		
+
+		// イベントが取得できるようにFocusを有効にする 
+		setFocusable(true);
+	
+		// サウンドデータの読み込み(res/raw/pon.wav)
+		mMp = MediaPlayer.create(context, R.raw.pon);	
 		// Callbackを登録する
 		getHolder().addCallback(this);
+
 	}
 
 	@Override
@@ -45,20 +55,32 @@ class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 	public void surfaceCreated(SurfaceHolder holder) {
 		Log.i("SURFACE","surfaceCreaded()");
 		
-		// Canvasを取得する
-		Canvas canvas = holder.lockCanvas();
+	}
 
-		// 背景を青にする
-		canvas.drawColor(Color.BLUE);
-		
-		// 描画するための線の色を設定
-		Paint mainPaint = new Paint();
-		mainPaint.setStyle(Paint.Style.FILL);
-		mainPaint.setARGB(255, 255, 255, 100);
-		
-		canvas.drawLine(0, 0, 50, 50, mainPaint);
 
-		// 画面に描画をする
-		holder.unlockCanvasAndPost(canvas);
+	/**
+	 * タッチイベント
+	 */
+	public boolean onTouchEvent(MotionEvent event) {
+		// タッチした時に実行
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			// 音の再生ポジションを0に設定
+			mMp.seekTo(0);
+			// 音の再生を開始
+			mMp.start();
+		} 
+		// タッチを話した時に実行
+		else if (event.getAction() == MotionEvent.ACTION_UP) {
+			// 音の停止
+			mMp.stop();
+			
+			// 一度再生をstop()した場合は、再び音を再生する場合は、prepare()を呼び出す必要がある
+			try {
+				mMp.prepare();
+			} catch (Exception e) {
+
+			}
+		}
+		return true;
 	}
 }
